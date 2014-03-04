2a9fdb558ad886438fcc968f6f775a1aa0fbdb4a
Nataly <natalyasvd@gmail.com>
2014-03-04 14:08:41 +0200

    fix

---
+++ config/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key = "aczfrrdb20yfxjd"
+  config.dropbox_app_secret = "kdhomojl2wy7mi8"
+  config.dropbox_access_token = "hwlmu54z0pswjmm5"
+  config.dropbox_access_token_secret ="oagpctrk47gar2m"
+  config.dropbox_user_id = "273376079"
+  config.dropbox_access_type = "dropbox"
+end
