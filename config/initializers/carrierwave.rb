0e0078ce1b2cf38ba3fe684a8aeed666e5d646d0
Nataly <natalyasvd@gmail.com>
2014-03-04 12:37:29 +0200

    aaa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = ENV["aczfrrdb20yfxjd"]
-  config.dropbox_app_secret = ENV["kdhomojl2wy7mi8"]
-  config.dropbox_access_token = ENV["hwlmu54z0pswjmm5"]
-  config.dropbox_access_token_secret = ENV["oagpctrk47gar2m"]
-  config.dropbox_user_id = ENV["273376079"]
+  config.dropbox_app_key = "aczfrrdb20yfxjd"
+  config.dropbox_app_secret = "kdhomojl2wy7mi8"
+  config.dropbox_access_token = "hwlmu54z0pswjmm5"
+  config.dropbox_access_token_secret ="oagpctrk47gar2m"
+  config.dropbox_user_id = "273376079"
   config.dropbox_access_type = "app_folder"
 end
