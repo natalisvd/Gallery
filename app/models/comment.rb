257f9551b44de7689592788dd905ede569fc1b2f
Nataly <natalyasvd@gmail.com>
2014-03-07 11:53:42 +0200

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   has_many :events, :as => :eventable
 
-  validates :body, :length => {:minimum => 1, :maximum => 100}
+  validates :body, :length => {:minimum => 1}
 
 
 end
